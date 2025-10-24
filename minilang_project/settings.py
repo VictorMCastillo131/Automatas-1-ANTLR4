09175c33937e8114189bbc6865b1732b9c6921f0
VictorMCastillo131 <victormanuelcastilloduran02@gmail.com>
2025-10-23 22:23:23 -0600

    Fix: Clean requirements.txt and secure SECRET_KEY

--- minilang_project/settings.py
+++ minilang_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e62osqk9es)chi_xhndo_pdkj*1y1+b0=)+=y0d_&wwn)7d2@!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
